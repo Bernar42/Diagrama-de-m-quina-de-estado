bdf6a70f0449a3a906e42b0fc4db46ce302f0e0f
Bernar42 <facundobernardini42@gmail.com>
2022-10-30 21:04:22 -0300

    Algo funcional
    Realicé la primera versión medianamente funcional del programa. Debo ver más información de como usar el archivo config.conf

--- TP1_MAQUINAS_DE_ESTADO/main.c
+++ TP1_MAQUINAS_DE_ESTADO/main.c
@@ -2,11 +2,23 @@
 #include "my_lib.h"
 
 int main(void) {
-    struct nivel bomba1;
 
-    int i=0;
-    imprimir();
+    struct nivel sensor1;
+    //seteo los niveles de los sesnores manualmante
 
-    printf("i %i", i);
+    sensor1.nivel_min=1;
+    sensor1.nivel_max=0;
+
+    while (sensor1.nivel_min=1 && sensor1.nivel_max=0)
+    {
+        bomba1=ON;
+        return 1;
+    }
+    while (sensor1.nivel_min=0 && sensor1.nivel_max=1)
+    {
+        bomba1=OFF;
+        return 1;
+    }
+
   return 0;
 }
--- TP1_MAQUINAS_DE_ESTADO/my_lib.h
+++ TP1_MAQUINAS_DE_ESTADO/my_lib.h
@@ -2,16 +2,25 @@
 #define MY_LIB_H
 #define ON 1
 #define OFF 0
-
 #include <stdio.h>
 
+  int bomba1=0;
 
-struct nivel {
- char nivel;
- char nivel_set;
+struct nivel { //sensor de nivel
+ char nivel_min;
+ char nivel_max;
 };
+void bomba1_on (void);
 
+void bomba1_on (void)
+{
+    bomba1=ON;
+}
+void bomba1_off (void);
+void bomba1_off (void)
+{
+    bomba1=OFF;
+}
 
-//aca colocare funciones y sus prototipos
 
 #endif // MY_LIB_H
