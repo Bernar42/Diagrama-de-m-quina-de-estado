d8b348c04a51756a833b63d76828e0977791d2d0
Bernar42 <facundobernardini42@gmail.com>
2022-11-14 15:38:48 -0300

    Comenté más lo que hice

--- TP1_MAQUINAS_DE_ESTADO/main.c
+++ TP1_MAQUINAS_DE_ESTADO/main.c
@@ -6,15 +6,15 @@ int main(void) {
     struct nivel sensor1;
     //seteo los niveles de los sensores manualmante
 
-    sensor1.nivel_min=1;
-    sensor1.nivel_max=0;
+    sensor1.nivel_min=1;// si 0, indica que hay agua
+    sensor1.nivel_max=0;// si 0, indica que hay agua
     sensor1.nivel_cisterna=1;
 
-    int nivel_actual=0;
+    int nivel_actual=0;// lo use como aux para guardar el valor devuelto por la funcion "obtener_nivel"
 
     while (1)
 {
-        nivel_actual=obtener_nivel(sensor1.nivel_max);
+        nivel_actual=obtener_nivel(sensor1.nivel_max);// le paso el valor guardado en max, es un valor obtenido de los sensores
         while(sensor1.nivel_cisterna!=0)
     {
          encendido_automatico_bombo(nivel_actual, sensor1.nivel_cisterna);
